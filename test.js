526f5dfb13c004ac9c5892d40e234adaa84119be
mo-jo-dev <b210055@nitsikkim.ac.in>
2025-01-10 22:52:35 +0530

    added test

---
+++ test.js
@@ -0,0 +1,4 @@
+// Wriet a function to give me the details of callback, promise and async await
+// function getDetails() {  }                                              // 1. Callback   2. Promise   3. Async Await     
+// getDetails();                                                        // 1. Callback   2. Promise   3. Async Await
+                    
